4ab360ecc1c75c890f10be46e800d26ccc348374
Lukas Schmidt <lightsprint09@users.noreply.github.com>
2020-06-30 10:45:11 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
     targets: [
         .binaryTarget(name: "AutomergeRSBackend",
                       url: "https://github.com/lightsprint09/automerge-rs/releases/download/v0.0.3/AutomergeRSBackend.xcframework.zip",
-                      checksum: "b86e04e80597222c0a7fd5a9a8a67ba518c502f065c8d5013596dcfc9abf7beb")
+                      checksum: "d22cc828f854e058960969a375011bcb14edf69827baaff30172fee21c67f848")
     ]
 )
